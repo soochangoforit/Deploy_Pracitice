21c534b9af4e78e1443ce6f029cdd534167d3c9f
soochan <tncksdl05@gmail.com>
2022-09-23 16:47:33 +0900

    initial commit

---
+++ src/main/java/test/test_deploy/TestDeployApplication.java
@@ -0,0 +1,13 @@
+package test.test_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/test_deploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package test.test_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
