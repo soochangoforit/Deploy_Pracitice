fb3443903deab2ee2d3984d38ff0a069015db43f
soochan <tncksdl05@gmail.com>
2022-09-24 16:25:06 +0900

    add response body annotation on controller

--- src/main/java/test/test_deploy/controller/TestController.java
+++ src/main/java/test/test_deploy/controller/TestController.java
@@ -2,11 +2,13 @@ package test.test_deploy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
     @GetMapping("/")
+    @ResponseBody
     public String test(){
         return "test gg, testing s3 , testing 3 for s3, testing code deploy";
     }
