0f586583e87fd9dbacf4b3c22323081a279e5da3
soochan <tncksdl05@gmail.com>
2022-09-25 10:42:19 +0900

    test12

--- src/main/java/test/test_deploy/TestDeployApplication.java
+++ src/main/java/test/test_deploy/TestDeployApplication.java
@@ -2,6 +2,7 @@ package test.test_deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TestDeployApplication {
