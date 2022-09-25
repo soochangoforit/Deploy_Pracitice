1afd56efa6b8f00ab95c5f8651f30ff58a24df9f
soochan <tncksdl05@gmail.com>
2022-09-25 09:52:20 +0900

    test11

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -20,6 +20,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/test-deploy/build/libs/*.jar > /home/ec2-user/${TARGET_PORT}.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/test-deploy/build/libs/* > /home/ec2-user/${TARGET_PORT}.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
